08ba9b95cd4ff973d7460a37dd2e54f153e1a02b
rise0chen <rise0chen@163.com>
2022-12-14 09:36:28 +0800

    fix: none

--- cmod-macros/src/backend/none/methods.rs
+++ cmod-macros/src/backend/none/methods.rs
@@ -1,5 +1,17 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ImplItem, ItemImpl};
 
 pub fn cmod_methods(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    input
+    let mut input = parse_macro_input!(input as ItemImpl);
+    input.attrs.clear();
+    input.items.iter_mut().for_each(|ii| match ii {
+        ImplItem::Method(md) => {
+            md.attrs.clear();
+        }
+        _ => (),
+    });
+    TokenStream::from(quote!(
+        #input
+    ))
 }
