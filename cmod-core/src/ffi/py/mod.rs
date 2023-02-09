de23843add472836cf8ab8d8dc30cd362da41fd2
rise0chen <rise0chen@163.com>
2023-02-09 15:31:01 +0800

    feat: pyo3-asyncio

--- cmod-core/src/ffi/py/mod.rs
+++ cmod-core/src/ffi/py/mod.rs
@@ -4,9 +4,14 @@ pub use self::serde::{FromFfi, ToFfi};
 use core::future::Future;
 use pyo3::exceptions::PyTypeError;
 pub use pyo3::prelude::*;
+use tokio::runtime::Runtime;
 
 pub fn map_err(msg: String) -> PyErr {
     PyTypeError::new_err(msg)
+}
+
+pub fn init_runtime(runtime: &'static Runtime) -> Result<(), ()> {
+    pyo3_asyncio::tokio::init_with_runtime(runtime)
 }
 
 pub fn block_on<F, T>(py: Python, f: F) -> PyResult<T>
@@ -15,5 +20,5 @@ where
     T: Send + Sync + 'static,
 {
     let _ = py;
-    futures_executor::block_on(f)
+    pyo3_asyncio::tokio::get_runtime().block_on(f)
 }
